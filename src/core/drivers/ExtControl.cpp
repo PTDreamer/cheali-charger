b319d94ad2f0b98f672dd8e6cedeaa50501bf2c1
Jose Barros <josemanuelbarros@gmail.com>
2019-12-27 14:58:00 +0000

    ExtControl work

--- src/core/drivers/ExtControl.cpp
+++ src/core/drivers/ExtControl.cpp
@@ -1,61 +1,144 @@
 /*
- * ExtControl.cpp
+ * ExtControl::cpp
  *
  *  Created on: Nov 22, 2019
  *      Author: jose
  */
 
 #include "ExtControl.h"
-#include "Keyboard.h"
+#ifndef QT
 #include "Settings.h"
 #include "Hardware.h"
 #include <string.h>
 #include "memory.h"
 #include "eeprom.h"
 #include "Serial.h"
-#include "Buzzer.h"
-#include "SerialLog.h"
 #include "Time.h"
+#include "TheveninMethod.h"
+#include "StackInfo.h"
+#include "Balancer.h"
+#include "Monitor.h"
+#endif
+#ifdef ENABLE_EXTERNAL_CONTROL
 
-#ifdef ENABLE_SERIAL_CONTROL
+#define MAX_RESENDS	5
+#define SEND_TIMEOUT 1000
+#define VIRTUAL_SEND_FREQUENCY 5000
+#define EXTRA_SEND_FREQUENCY 8000
+#define MAX_BACKLOG 10
 
-ExtControl::ExtControl() {
-	currentProcessStatus = WAITING_MAGIC1;
-	currentMessageNumber_sent = 0;
-	currentMessageNumber_received = 0;
-	messageSizes[ACK] = 1;//messageNumber
-	messageSizes[NACK] = 1;//messageNumber
+namespace ExtControl {
+struct fifo {
+	uint8_t head;
+	uint8_t tail;
+	uint8_t elements;
+	uint8_t buffer[MAX_BACKLOG];
+};
+void sendRealValues();
+void sendVirtualValues();
+void sendExtraValues();
+void pushFifo(uint8_t, fifo *);
+int8_t popFifo(fifo *);
+int8_t fifoSize(fifo *f) {return f->elements;}
+static uint8_t rx_message[sizeof(largestSize)];
+static uint8_t tx_message[sizeof(largestSize)];
+static uint8_t currentMessageNumber_sent = 0;
+static uint8_t currentMessageNumber_received = 0;
+static message_type currentTypeSent = ACK;
+static uint8_t currentSentAcked = true;
+static uint16_t currentSentTime = 0;
+command_type currentCommand = CMD_IDLE;
+commandDataType commandData = {};
+ProgramData::Battery volatileBattery;
+bool isSendingAllBattery = false;
+error_type current_error = ERROR_NONE;
+static bool isInit = false;
+
+static uint8_t messageSizes[LAST_MESSAGE_TYPE];
+static packet_process_status currentProcessStatus = WAITING_MAGIC1;
+current_state_type currentState = STATE_NOT_CONTROLING;
+static fifo statesBacklog;
+static fifo errorsBacklog;
+static fifo programsBacklog;
+
+static uint16_t lastExtraSentTime = 0;
+static uint16_t lastVirtualSentTime = 0;
+Program::ProgramType currentProgram = Program::LAST_PROGRAM_TYPE;
+#ifdef QT
+helper *help;
+#endif
+}
+
+void ExtControl::init() {
+	messageSizes[ACK] = 1; //messageNumber
+	messageSizes[NACK] = 1; //messageNumber
 	messageSizes[GET_OPTIONS] = 0;
 	messageSizes[GET_BAT_SETTINGS] = 1; //index
 	messageSizes[SET_VOLATILE_BAT] = sizeof(ProgramData::Battery); //TODO
 	messageSizes[PUT_OPTIONS] = sizeof(Settings);
+	messageSizes[SET_OPTIONS] = sizeof(Settings);
 	messageSizes[PUT_BAT_SETTINGS] = sizeof(batteryInfo);
-	messageSizes[COMMAND] = 1;
-	currentState = STATE_NOT_CONTROLING;
-	isUnderControl = 0;
-	uint8_t currentTypeSent = 0;
-	uint8_t currentSentAcked = 1;
-}
-void ExtControl::powerOn() {
-
+	messageSizes[SET_BAT_SETTINGS] = sizeof(batteryInfo); //index
+	messageSizes[COMMAND] = sizeof(commandType);
+	messageSizes[STATE_CHANGED] = 1;
+	messageSizes[PROGRAM_CHANGED] = 1;
+	messageSizes[ERROR_OCORRED] = 1;
+	messageSizes[PUT_REAL_INPUTS] = sizeof(realInputs);
+	messageSizes[PUT_VIRTUAL_INPUTS] = sizeof(virtualInputs);
+#ifdef EXTCONTROL_DEBUG
+	messageSizes[DEBUG_MSG] = 10;
+#endif
+	isInit = true;
 }
 void ExtControl::doIdle() {
+	uint8_t temp;
+    static uint16_t analogCount;
 	uint8_t dataReady = 0;
-	if (!settings.serialControl)
+	if (!isInit)
+		init();
+	if (settings.UART != Settings::ExtControl)
 		return;
-	if (!isUnderControl) {
-		uint8_t key;
-		//BUTTON_INC
-		key = Keyboard::getLast();
-		if (key == BUTTON_INC && Keyboard::isExtSerialCommand()) {
-			Buzzer::soundProgramComplete();
-			SerialLog::serialBegin();
-			SerialLog::printString("INIT");
-			Serial::beginRx(settings.getUARTspeed());
-			isUnderControl = 1;
+	if(currentState == STATE_NOT_CONTROLING)
+		return;
+	if (!currentSentAcked
+			&& Time::diffU16(currentSentTime, Time::getMilisecondsU16()) > SEND_TIMEOUT)
+		sendPackage(tx_message, currentTypeSent, true);
+#ifndef QT
+	else if (currentSentAcked && isSendingAllBattery)
+		processGetBatSettings(ALL_BAT_SETTINGS);
+	else if (currentSentAcked && fifoSize(&statesBacklog) > 0) {
+		temp = popFifo(&statesBacklog);
+		sendPackage((uint8_t *) &temp, ExtControl::STATE_CHANGED);
+	}
+	else if (currentSentAcked && fifoSize(&errorsBacklog) > 0) {
+		temp = popFifo(&errorsBacklog);
+		sendPackage((uint8_t *) &temp, ExtControl::ERROR_OCORRED);
+	}
+	else if (currentSentAcked && fifoSize(&programsBacklog) > 0) {
+		temp = popFifo(&programsBacklog);
+		sendPackage((uint8_t *) &temp, ExtControl::PROGRAM_CHANGED);
+	}
+	if(currentSentAcked && (currentState == STATE_RUNNING || currentState == STATE_SETUP_COMPLETED)) {
+		    if(!AnalogInputs::isPowerOn()) {
+		        analogCount = 0;
+	} else {
+		uint16_t c = AnalogInputs::getFullMeasurementCount();
+		if (analogCount != c) {
+			analogCount = c;
+			sendRealValues();
+		} else if (Time::diffU16(lastVirtualSentTime,
+				Time::getMilisecondsU16()) > VIRTUAL_SEND_FREQUENCY) {
+			lastVirtualSentTime = Time::getMilisecondsU16();
+			sendVirtualValues();
+		} else if (Time::diffU16(lastExtraSentTime,
+				Time::getMilisecondsU16()) > EXTRA_SEND_FREQUENCY) {
+			lastExtraSentTime = Time::getMilisecondsU16();
+			sendExtraValues();
 		}
 	}
-	if (isUnderControl) {
+	 }
+#endif
+	 if(true){//TODO MAYBE TAKE OUT THE IF
 		int c = -1;
 		do {
 			c = Serial::read();
@@ -64,20 +147,18 @@ void ExtControl::doIdle() {
 			}
 		} while (c != -1);
 	}
-
 }
 uint8_t ExtControl::processInputByte(int c) {
 	static uint8_t currentData = 0;
 	static uint8_t currentMsgSize = 0;
 	static uint16_t currentCrc = 0xffff;
-	static uint8_t currentNumber = 0;
 	static message_type msgType = ACK;
 	static uint16_t receivedCrc = 0;
-	SerialLog::printInt(currentProcessStatus);
 	switch (currentProcessStatus) {
 	case WAITING_MAGIC1:
-		if (c == MAGIC1)
+		if (c == MAGIC1) {
 			currentProcessStatus = WAITING_MAGIC2;
+		}
 		break;
 	case WAITING_MAGIC2:
 		if (c == MAGIC2) {
@@ -87,20 +168,19 @@ uint8_t ExtControl::processInputByte(int c) {
 		}
 		break;
 	case WAITING_NUMBER:
-		currentNumber = c;
+		currentMessageNumber_received = c;
 		currentCrc = crc16_update(currentCrc, c);
 		currentProcessStatus = WAITING_TYPE;
 		break;
 	case WAITING_TYPE:
-		if(c > (LAST_MESSAGE_TYPE -1)) {
+		if (c > (LAST_MESSAGE_TYPE - 1)) {
 			currentProcessStatus = WAITING_MAGIC1;
 			return 0;
 		}
 		currentMsgSize = messageSizes[c];
-		msgType = (message_type)c;
+		msgType = (message_type) c;
 		currentCrc = crc16_update(currentCrc, c);
-		SerialLog::printUInt(c);
-		if(currentMsgSize)
+		if (currentMsgSize)
 			currentProcessStatus = WAITING_DATA;
 		else
 			currentProcessStatus = WAITING_CRC1;
@@ -109,7 +189,7 @@ uint8_t ExtControl::processInputByte(int c) {
 		rx_message[currentData] = c;
 		currentCrc = crc16_update(currentCrc, c);
 		++currentData;
-		if(currentData == currentMsgSize) {
+		if (currentData == currentMsgSize) {
 			currentProcessStatus = WAITING_CRC1;
 		}
 		break;
@@ -121,30 +201,47 @@ uint8_t ExtControl::processInputByte(int c) {
 		currentProcessStatus = WAITING_MAGIC1;
 		receivedCrc = receivedCrc << 8;
 		receivedCrc = receivedCrc | c;
-		if(receivedCrc == currentCrc) {
-			sendPackage(&currentNumber, ACK);
+		if (receivedCrc == currentCrc) {
+			if (msgType != ACK && msgType != NACK && msgType != PUT_REAL_INPUTS &&  msgType != PUT_VIRTUAL_INPUTS && msgType != PUT_EXTRA_VALUES)
+				sendPackage(&currentMessageNumber_received, ACK);
+#ifdef QT
+			help->trigger_messageReceived(msgType, rx_message);
+#endif
 			switch (msgType) {
-				case GET_OPTIONS:
-					processGetOptions();
-					break;
-				case GET_BAT_SETTINGS:
-					processGetBatSettings(rx_message[0]);
-					break;
-				case SET_VOLATILE_BAT:
-					processVolatileBat();
-					break;
-				default:
-					break;
+			case ACK:
+				if (rx_message[0] == (currentMessageNumber_sent - 1))
+					currentSentAcked = true;
+				break;
+			case GET_OPTIONS:
+				processGetOptions();
+				break;
+			case SET_OPTIONS:
+				processSetOptions();
+				break;
+			case GET_BAT_SETTINGS:
+				processGetBatSettings(rx_message[0]);
+				break;
+			case SET_BAT_SETTINGS:
+				processSetBatSettings(rx_message);
+				break;
+			case SET_VOLATILE_BAT:
+				processVolatileBat();
+				break;
+			case COMMAND:
+				processCommand();
+				break;
+			default:
+				break;
 			}
+#ifdef QT
+			help->trigger_messageHandled(msgType);
+#endif
 			return 1;
-		}
-		else {
-			SerialLog::printString("wrong CRC");
-			sendPackage(&currentNumber, NACK);
+		} else {
+			//sendPackage(&currentMessageNumber_received, NACK);
 		}
 		break;
 	default:
-		return 0;
 		break;
 	}
 	return 0;
@@ -152,24 +249,154 @@ uint8_t ExtControl::processInputByte(int c) {
 
 void ExtControl::processVolatileBat() {
 	memcpy(&volatileBattery, rx_message, sizeof(ProgramData::Battery));
-	ProgramData::check();
 }
+
+void ExtControl::processCommand() {
+	ExtControl::commandType command;
+	memcpy(&command, rx_message, sizeof(ExtControl::commandType));
+	currentCommand = (command_type)command.command;
+	commandData = command.data;
+	switch (command.command) {
+	case ExtControl::CMD_IDLE:
+		break;
+	case ExtControl::CMD_STOP:
+		break;
+	case ExtControl::CMD_SETUP:
+		break;
+	case ExtControl::CMD_START:
+		break;
+	default:
+		break;
+	}
+}
+#ifndef QT
+void ExtControl::setState(ExtControl::current_state_type state) {
+	if(currentState == STATE_NOT_CONTROLING && state != STATE_BEGIN)
+		return;
+	if (state != currentState) {
+		if(!sendPackage((uint8_t *) &state, ExtControl::STATE_CHANGED)) {
+			pushFifo(state, &statesBacklog);
+		}
+	}
+	ExtControl::current_state_type oldState = state;
+	switch (state) {
+	case ExtControl::STATE_BEGIN:
+		Serial::beginRx(settings.getUARTspeed());
+		Serial::begin(settings.getUARTspeed());
+		currentState = STATE_IDLE;
+		break;
+	case ExtControl::STATE_STOPPED:
+		if (currentCommand == ExtControl::CMD_STOP) {
+			currentState = ExtControl::STATE_STOPPED;
+			currentCommand = ExtControl::CMD_IDLE;
+		}
+		break;
+	case ExtControl::STATE_IDLE:
+		currentState = ExtControl::STATE_IDLE;
+		break;
+	case ExtControl::STATE_NOT_CONTROLING:
+		if(oldState != STATE_NOT_CONTROLING) {
+			Serial::endRx();
+			Serial::end();
+		}
+		currentState = ExtControl::STATE_NOT_CONTROLING;
+		currentCommand = CMD_IDLE;
+		break;
+	case ExtControl::STATE_COMPLETED:
+		currentState = ExtControl::STATE_COMPLETED;
+		break;
+	case ExtControl::STATE_SETUP_COMPLETED:
+		currentState = ExtControl::STATE_SETUP_COMPLETED;
+		currentCommand = ExtControl::CMD_IDLE;
+		break;
+	case ExtControl::STATE_RUNNING:
+		currentState = ExtControl::STATE_RUNNING;
+		break;
+	default:
+		break;
+	}
+	if (oldState != currentState) {
+		if(!sendPackage((uint8_t *) &currentState, ExtControl::STATE_CHANGED)) {
+			pushFifo(currentState, &statesBacklog);
+		}
+	}
+}
+
+void ExtControl::setError(ExtControl::error_type error) {
+	if (current_error != error) {
+		if(!sendPackage((uint8_t *) &error, ExtControl::ERROR_OCORRED)) {
+			pushFifo(error, &errorsBacklog);
+		}
+	}
+	current_error = error;
+}
+#endif
 void ExtControl::processGetOptions() {
-	sendPackage((uint8_t*)&settings, PUT_OPTIONS);
+	sendPackage((uint8_t*) &settings, PUT_OPTIONS);
+}
+
+void ExtControl::processSetOptions() {
+	memcpy(&settings, rx_message, sizeof(Settings));
+	Settings::save();
 }
 void ExtControl::processGetBatSettings(uint8_t index) {
+	static uint8_t sendAllIndex = 0;
 	batteryInfo msg;
-	msg.index = index;
-    eeprom::read(msg.battery, &eeprom::data.battery[index]);
-    sendPackage((uint8_t*)&msg, PUT_BAT_SETTINGS);
+	if (index == ALL_BAT_SETTINGS) {
+		if (!isSendingAllBattery) {
+			isSendingAllBattery = true;
+			sendAllIndex = 0;
+		} else
+			++sendAllIndex;
+		msg.index = sendAllIndex;
+		if (sendAllIndex == MAX_PROGRAMS) {
+			isSendingAllBattery = false;
+			return;
+		}
+		eeprom::read(msg.battery, &eeprom::data.battery[sendAllIndex]);
+	} else {
+		msg.index = index;
+		eeprom::read(msg.battery, &eeprom::data.battery[index]);
+	}
+	sendPackage((uint8_t*) &msg, PUT_BAT_SETTINGS);
 }
-void ExtControl::sendPackage(uint8_t *buffer, message_type type) {
-	if(!currentSentAcked)
-		return;
-	currentSentAcked = 0;
+
+void ExtControl::processSetBatSettings(uint8_t *buffer) {
+	ExtControl::batteryInfo info;
+	memcpy(&info, buffer, sizeof(ExtControl::batteryInfo));
+	eeprom::write(&eeprom::data.battery[info.index], info.battery);
+	eeprom::restoreProgramDataCRC();
+}
+bool ExtControl::sendPackage(uint8_t *buffer, message_type type,
+		bool isResend, bool requireACK) {
+#ifdef QT
+	help->trigger_messageSent(type, buffer, isResend);
+#endif
+	static uint8_t resendCounter = 0;
+#ifdef EXTCONTROL_DEBUG
+	if (!currentSentAcked && !isResend && (type != DEBUG_MSG))
+		return false;
+#else
+	if (!currentSentAcked && !isResend)
+		return false;
+#endif
+	if (isResend) {
+		++resendCounter;
+		if (resendCounter > MAX_RESENDS) {
+			currentSentAcked = 1;
+			return false;
+		}
+		--currentMessageNumber_sent;
+	} else
+		resendCounter = 0;
+	if (type == ACK || !requireACK)
+		currentSentAcked = 1;
+	else
+		currentSentAcked = 0;
 	currentTypeSent = type;
 	currentSentTime = Time::getMilisecondsU16();
-	memcpy(tx_message, buffer, messageSizes[type]);
+	if (!isResend)
+		memcpy(tx_message, buffer, messageSizes[type]);
 	uint16_t crc = 0xffff;
 	Serial::write(MAGIC1);
 	Serial::write(MAGIC2);
@@ -178,12 +405,13 @@ void ExtControl::sendPackage(uint8_t *buffer, message_type type) {
 	++currentMessageNumber_sent;
 	Serial::write(type);
 	crc = crc16_update(crc, type);
-	for(uint8_t i = 0; i < messageSizes[type]; ++i) {
+	for (uint8_t i = 0; i < messageSizes[type]; ++i) {
 		crc = crc16_update(crc, buffer[i]);
 		Serial::write(buffer[i]);
 	}
 	Serial::write(crc >> 8);
 	Serial::write(crc & 0xff);
+	return true;
 }
 uint16_t ExtControl::crc16_update(uint16_t crc, uint8_t a) {
 	int i;
@@ -198,9 +426,158 @@ uint16_t ExtControl::crc16_update(uint16_t crc, uint8_t a) {
 
 	return crc;
 }
-void ExtControl::powerOff() {
 
+uint8_t ExtControl::getCommand() {
+	return currentCommand;
 }
 
-ExtControl extControl;
+ExtControl::commandDataType ExtControl::getCommandData() {
+	return commandData;
+}
+
+ProgramData::Battery * ExtControl::getVolatileBattery() {
+	return &volatileBattery;
+}
+
+bool ExtControl::externalControlRequested() {
+	return currentCommand != CMD_IDLE;
+}
+bool ExtControl::getConnected() {
+	return currentState != STATE_NOT_CONTROLING;
+} //TODO
+#ifdef EXTCONTROL_DEBUG
+#include "string.h"
+namespace ExtControl {
+	char debug[10];
+}
+	void ExtControl::sendDebug(char *val) {
+		strcpy(debug, val);
+		sendPackage((uint8_t*)&debug, DEBUG_MSG);
+	}
+#endif
+
+#ifndef QT
+
+void ExtControl::parseErrorFromString(const char *s) {
+	if(s == Monitor::string_batteryDisconnected)
+		setError(ERROR_MON_BATT_DISC);
+	else if(s == Monitor::string_internalTemperatureToHigh)
+		setError(ERROR_MON_INT_TEMP_CUTOFF);
+	else if(s == Monitor::string_balancePortDisconnected)
+			setError(ERROR_MON_BALANCER_DISC);
+	else if(s == Monitor::string_outputCurrentToHigh)
+			setError(ERROR_MON_HW_FAILURE);
+	else if(s == Monitor::string_inputVoltageToLow)
+			setError(ERROR_MON_V_TOO_LOW);
+	else if(s == Monitor::string_capacityLimit)
+			setError(ERROR_MON_CAPACITY_CUTOFF);
+	else if(s == Monitor::string_timeLimit)
+			setError(ERROR_MON_TIME_LIMIT);
+	else if(s == Monitor::string_externalTemperatureCutOff)
+			setError(ERROR_MON_EXT_TEMP_CUTOFF);
+}
+
+void ExtControl::sendRealValues() {
+	realInputs i;
+	i.Idischarge = AnalogInputs::getRealValue(AnalogInputs::Idischarge);
+	i.IdischargeSet = AnalogInputs::getRealValue(AnalogInputs::IdischargeSet);
+	i.Ismps = AnalogInputs::getRealValue(AnalogInputs::Ismps);
+	i.IsmpsSet = AnalogInputs::getRealValue(AnalogInputs::IsmpsSet);
+	i.Textern = AnalogInputs::getRealValue(AnalogInputs::Textern);
+	i.Tintern = AnalogInputs::getRealValue(AnalogInputs::Tintern);
+	i.Vb0_pin = AnalogInputs::getRealValue(AnalogInputs::Vb0_pin);
+	i.Vb1_pin = AnalogInputs::getRealValue(AnalogInputs::Vb1_pin);
+	i.Vb2_pin = AnalogInputs::getRealValue(AnalogInputs::Vb2_pin);
+	i.Vb3_pin = AnalogInputs::getRealValue(AnalogInputs::Vb3_pin);
+	i.Vb4_pin = AnalogInputs::getRealValue(AnalogInputs::Vb4_pin);
+	i.Vb5_pin = AnalogInputs::getRealValue(AnalogInputs::Vb5_pin);
+	i.Vb6_pin = AnalogInputs::getRealValue(AnalogInputs::Vb6_pin);
+	i.Vin = AnalogInputs::getRealValue(AnalogInputs::Vin);
+	i.VoutMux = AnalogInputs::getRealValue(AnalogInputs::VoutMux);
+	i.Vout_minus_pin = AnalogInputs::getRealValue(AnalogInputs::Vout_minus_pin);
+	i.Vout_plus_pin = AnalogInputs::getRealValue(AnalogInputs::Vout_plus_pin);
+	sendPackage((uint8_t*)&i, PUT_REAL_INPUTS, false, false);
+}
+
+void ExtControl::sendVirtualValues() {
+	virtualInputs i;
+	i.Cout = AnalogInputs::getRealValue(AnalogInputs::Cout);
+	i.Eout = AnalogInputs::getRealValue(AnalogInputs::Eout);
+	i.Iout = AnalogInputs::getRealValue(AnalogInputs::Iout);
+	i.Pout = AnalogInputs::getRealValue(AnalogInputs::Pout);
+	i.Vb1 = AnalogInputs::getRealValue(AnalogInputs::Vb1);
+	i.Vb2 = AnalogInputs::getRealValue(AnalogInputs::Vb2);
+	i.Vb3 = AnalogInputs::getRealValue(AnalogInputs::Vb3);
+	i.Vb4 = AnalogInputs::getRealValue(AnalogInputs::Vb4);
+	i.Vb5 = AnalogInputs::getRealValue(AnalogInputs::Vb5);
+	i.Vb6 = AnalogInputs::getRealValue(AnalogInputs::Vb6);
+	i.VbalanceInfo = AnalogInputs::getRealValue(AnalogInputs::VbalanceInfo);
+	i.Vbalancer = AnalogInputs::getRealValue(AnalogInputs::Vbalancer);
+	i.VobInfo = AnalogInputs::getRealValue(AnalogInputs::VobInfo);
+	i.Vout = AnalogInputs::getRealValue(AnalogInputs::Vout);
+	i.VoutBalancer = AnalogInputs::getRealValue(AnalogInputs::VoutBalancer);
+	i.deltaLastCount = AnalogInputs::getRealValue(AnalogInputs::deltaLastCount);
+	i.deltaTextern = AnalogInputs::getRealValue(AnalogInputs::deltaTextern);
+	i.deltaVout = AnalogInputs::getRealValue(AnalogInputs::deltaVout);
+	i.deltaVoutMax = AnalogInputs::getRealValue(AnalogInputs::deltaVoutMax);
+	sendPackage((uint8_t*)&i, PUT_VIRTUAL_INPUTS, false, false);
+}
+
+void ExtControl::sendExtraValues() {
+	extraValues i;
+    i.RthCell1 = TheveninMethod::getReadableRthCell(1);
+    i.RthCell1 = TheveninMethod::getReadableRthCell(2);
+    i.RthCell1 = TheveninMethod::getReadableRthCell(3);
+    i.RthCell1 = TheveninMethod::getReadableRthCell(4);
+    i.RthCell1 = TheveninMethod::getReadableRthCell(5);
+    i.RthCell1 = TheveninMethod::getReadableRthCell(6);
+    i.BattRth = TheveninMethod::getReadableBattRth();
+    i.WiresRth = TheveninMethod::getReadableWiresRth();
+    i.percentCompleted = Monitor::getChargeProcent();
+    i.ETATime = Monitor::getETATime();
+    i.Balance = Balancer::balance;
+    i.PID = hardware::getPIDValue();
+    i.NeverUsedStackSize = StackInfo::getNeverUsedStackSize();
+    i.FreeStack = StackInfo::getFreeStackSize();
+	sendPackage((uint8_t*)&i, PUT_EXTRA_VALUES, false, false);
+}
+void ExtControl::setCurrentProgram(Program::ProgramType prog) {
+	if(currentProgram != prog) {
+		if(!sendPackage((uint8_t*)&prog, PROGRAM_CHANGED)) {
+			pushFifo(prog, &programsBacklog);
+		}
+	}
+}
+
+void ExtControl::pushFifo(uint8_t val, fifo *f) {
+	  if(f->elements == MAX_BACKLOG) {
+	    return;
+	  }
+	  else {
+	    f->elements++;
+	    if(f->elements > 1) {
+	      f->tail++;
+	      f->tail %= MAX_BACKLOG;
+	    }
+	    //Store data into array
+	    f->buffer[f->tail] = val;
+	  }
+}
+
+int8_t ExtControl::popFifo(fifo *f) {
+	 if(f->elements == 0) {
+	    return -1;
+	  }
+	  else {
+	    f->elements--;
+	    uint8_t data = f->buffer[f->head];
+	    if(f->elements >= 1) {
+	      f->head++;
+	      f->head %= MAX_BACKLOG;
+	    }
+	    return data;
+	  }
+}
+
+#endif
 #endif
--- src/core/drivers/ExtControl.h
+++ src/core/drivers/ExtControl.h
@@ -2,64 +2,209 @@
 #define EXTCONTROL_H_
 
 #include <inttypes.h>
-#include <stddef.h>
+#ifndef QT
 #include <avr/io.h>
 #include "ProgramData.h"
-
+#include "Program.h"
+#include "Settings.h"
+#else
+#include <QObject>
+#include "serial.h"
+#endif
+//#define EXTCONTROL_DEBUG
 #define MAGIC1 0xFE
 #define MAGIC2 0xEF
-#define LOAD_VOLATILE_BATTERY 126
-#ifdef ENABLE_SERIAL_CONTROL
-class ExtControl
-{
-public:
-	enum command_type {CMD_START, CMD_SETUP, CMD_STOP, CMD_IDLE, LAST_COMMAND_TYPE};
-	enum current_state_type {STATE_NOT_CONTROLING, STATE_IDLE, STATE_STOPPED, STATE_ERROR, STATE_RUNNING, STATE_COMPLETED, STATE_SETUP_COMPLETED, LAST_STATE_TYPE};
-	ExtControl();
-    void powerOn();
-    void doIdle();
-    void powerOff();
-    uint8_t getCommand() { return currentCommand;}
-    uint8_t getCommandData() { return 0;}
-    uint8_t externalControlRequested() {return false;}
-    ProgramData::Battery * getVolatileBattery(){return &volatileBattery;}
-    void stopped() {currentState = STATE_STOPPED;}
-    void setState(current_state_type state) {currentState = state;}
-    bool getConnected() {return isUnderControl;} //TODO
-private:
-	uint8_t isUnderControl;
-	enum message_type {ACK, NACK, GET_OPTIONS, GET_BAT_SETTINGS, SET_VOLATILE_BAT,
-						PUT_OPTIONS, PUT_BAT_SETTINGS, COMMAND, LAST_MESSAGE_TYPE};
-	enum packet_process_status {WAITING_MAGIC1, WAITING_MAGIC2, WAITING_NUMBER, WAITING_TYPE,
-								WAITING_DATA, WAITING_CRC1, WAITING_CRC2, LAST_PROCESS_STATUS};
-	packet_process_status currentProcessStatus;
-	current_state_type currentState;
-	uint16_t crc16_update(uint16_t crc, uint8_t a);
-	uint8_t messageSizes[LAST_MESSAGE_TYPE];
-	command_type currentCommand;
-	ProgramData::Battery volatileBattery;
-	struct batteryInfo {
-		uint8_t index;
-		ProgramData::Battery battery;
-	};
-	struct largestSize {
-		union {
+#define ALL_BAT_SETTINGS 255
+#ifdef ENABLE_EXTERNAL_CONTROL
+
+namespace ExtControl {
+#ifdef EXTCONTROL_DEBUG
+extern char debug[];
+extern void sendDebug(char *);
+#endif
+
+struct batteryInfo {
+	uint8_t index;
+	ProgramData::Battery battery;
+};
+
+struct extraValues {
+	uint16_t RthCell1;
+	uint16_t RthCell2;
+	uint16_t RthCell3;
+	uint16_t RthCell4;
+	uint16_t RthCell5;
+	uint16_t RthCell6;
+	uint16_t BattRth;
+	uint16_t WiresRth;
+	uint8_t percentCompleted;
+	uint32_t ETATime;
+	uint16_t FreeStack;
+	uint16_t NeverUsedStackSize;
+	uint16_t PID;
+	uint16_t Balance;
+};
+struct realInputs {
+	uint16_t Vout_plus_pin;
+	uint16_t Vout_minus_pin;
+	uint16_t Ismps;
+	uint16_t Idischarge;
+	uint16_t VoutMux;
+	uint16_t Tintern;
+	uint16_t Vin;
+	uint16_t Textern;
+	uint16_t Vb0_pin;
+	uint16_t Vb1_pin;
+	uint16_t Vb2_pin;
+	uint16_t Vb3_pin;
+	uint16_t Vb4_pin;
+	uint16_t Vb5_pin;
+	uint16_t Vb6_pin;
+	uint16_t IsmpsSet;
+	uint16_t IdischargeSet;
+};
+
+struct virtualInputs {
+	uint16_t Vout;
+	uint16_t Vbalancer;
+	uint16_t VoutBalancer;
+	uint16_t VobInfo;
+	uint16_t VbalanceInfo;
+	uint16_t Iout;
+	uint16_t Pout;
+	uint16_t Cout;
+	uint16_t Eout;
+	uint16_t deltaVout;
+	uint16_t deltaVoutMax;
+	uint16_t deltaTextern;
+	uint16_t deltaLastCount;
+	uint16_t Vb1;
+	uint16_t Vb2;
+	uint16_t Vb3;
+	uint16_t Vb4;
+	uint16_t Vb5;
+	uint16_t Vb6;
+};
+
+struct largestSize {
+	union {
 		batteryInfo b;
 		Settings s;
-		};
+		virtualInputs v;
+		realInputs r;
 	};
-	uint8_t rx_message[sizeof(largestSize)];
-	uint8_t tx_message[sizeof(largestSize)];
-	uint8_t currentMessageNumber_sent;
-	uint8_t currentMessageNumber_received;
-	uint8_t currentTypeSent;
-	uint8_t currentSentAcked;
-	uint16_t currentSentTime;
-	void processGetOptions();
-	void processGetBatSettings(uint8_t index);
-	void sendPackage(uint8_t *buffer, message_type type);
-	void processVolatileBat();
-    uint8_t processInputByte(int c);
+};
+
+enum command_type {
+	CMD_START, CMD_SETUP, CMD_STOP, CMD_IDLE, LAST_COMMAND_TYPE
+};
+enum current_state_type {
+	STATE_NOT_CONTROLING,
+	STATE_BEGIN,
+	STATE_IDLE,
+	STATE_STOPPED,
+	STATE_ERROR,
+	STATE_RUNNING,
+	STATE_COMPLETED,
+	STATE_SETUP_COMPLETED,
+	LAST_STATE_TYPE
+};
+enum error_type {
+	ERROR_NONE = 0,
+	ERROR_BALANCE = 1,
+	ERROR_BATTERY = 2,
+	ERROR_STRATEGY,
+	ERROR_MON_BATT_DISC,
+	ERROR_MON_INT_TEMP_CUTOFF,
+	ERROR_MON_BALANCER_DISC,
+	ERROR_MON_HW_FAILURE,
+	ERROR_MON_V_TOO_LOW,
+	ERROR_MON_CAPACITY_CUTOFF,
+	ERROR_MON_TIME_LIMIT,
+	ERROR_MON_EXT_TEMP_CUTOFF,
+	LAST_ERROR_TYPE
+
+};
+
+extern void parseErrorFromString(const char*);
+//TODO
+enum message_type {
+	ACK,
+	NACK,
+	GET_OPTIONS,
+	GET_BAT_SETTINGS,
+	SET_BAT_SETTINGS,
+	SET_VOLATILE_BAT,
+	PUT_OPTIONS,
+	SET_OPTIONS,
+	PUT_BAT_SETTINGS,
+	COMMAND,
+	STATE_CHANGED,
+	ERROR_OCORRED,
+	PUT_REAL_INPUTS,
+	PUT_VIRTUAL_INPUTS,
+	PUT_EXTRA_VALUES,
+	PROGRAM_CHANGED,
+#ifdef EXTCONTROL_DEBUG
+	DEBUG_MSG,
+#endif
+	LAST_MESSAGE_TYPE
+};
+
+struct commandDataType {
+	uint8_t data1;
+	uint8_t data2;
+}__attribute__((packed));
+
+struct commandType {
+	uint8_t command;
+	commandDataType data;
+}__attribute__((packed));
+
+extern command_type currentCommand;
+extern commandDataType commandData;
+extern ProgramData::Battery volatileBattery;
+extern bool isSendingAllBattery;
+extern error_type current_error;
+
+extern current_state_type currentState;
+
+
+#ifdef QT
+extern helper *help;
+#endif
+void setError(error_type error);
+void init();
+void doIdle();
+uint8_t getCommand();
+commandDataType getCommandData();
+ProgramData::Battery * getVolatileBattery();
+void setState(current_state_type state);
+void setCurrentProgram(Program::ProgramType prog);
+bool getConnected(); //TODO
+uint8_t processInputByte(int c);
+bool sendPackage(uint8_t *buffer, message_type type, bool isResend = false, bool requireACK = true);
+
+bool externalControlRequested();
+enum packet_process_status {
+	WAITING_MAGIC1,
+	WAITING_MAGIC2,
+	WAITING_NUMBER,
+	WAITING_TYPE,
+	WAITING_DATA,
+	WAITING_CRC1,
+	WAITING_CRC2,
+	LAST_PROCESS_STATUS
+};
+
+uint16_t crc16_update(uint16_t crc, uint8_t a);
+
+void processGetOptions();
+void processSetOptions();
+void processGetBatSettings(uint8_t index);
+void processSetBatSettings(uint8_t *buffer);
+void processVolatileBat();
+void processCommand();
 
 //magic1
 //magic2
@@ -68,7 +213,6 @@ private:
 //data
 //crc1
 //crc2
-};
-extern ExtControl extControl;
+}
 #endif
 #endif /* EXTCONTROL_H_ */
